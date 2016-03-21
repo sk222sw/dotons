038929c4a91dff931cccf59d063463745a9f5a62
Sonny Kjellberg <sk222sw@student.lnu.se>
2016-03-21 19:57:01 +0000

    port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+console.log(process.env.PORT);
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Dotons\n');
-}).listen(process.env.PORT, process.env.IP);
+}).listen(3000);
