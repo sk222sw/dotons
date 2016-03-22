bc60041c32c1c49ef8233659ffef45b1bebd087a
Sonny <sk222sw@student.lnu.se>
2016-03-22 16:11:41 +0100

    set up node backend with express and proxied webpack frontend

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+var httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer();
+const app = express();
+
+const isProduction = process.env.NODE_ENV === "production";
+const port = isProduction ? process.env.PORT : 3000;
+const publicPath = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+if (!isProduction) {
+  var bundle = require("./server/bundle.js");
+  bundle();
+
+  app.all('/build/*', function (req, res) {
+    proxy.web(req, res, {
+        target: 'http://localhost:8080'
+    });
+  });
+}
+
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...');
+});
+
+app.listen(port, function () {
+  console.log("Server running on port:", port);
+});
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var Webpack = require('webpack');
+var path = require('path');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var buildPath = path.resolve(__dirname, 'public', 'build');
+var mainPath = path.resolve(__dirname, 'app', 'main.js');
+
+var config = {
+  devtool: "eval",
+  entry: [
+    "webpack/hot/dev-server",
+    "webpack-dev-server/client?http://localhost:8080",
+    mainPath
+  ],
+  output: {
+    path: buildPath,
+    filename: "bundle.js",
+    publicPath: "/build/"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: "babel",
+        exclude: [nodeModulesPath]
+      },
+      {
+        test: /\.css$/,
+        loader: "style!css"
+      }
+    ]
+  },
+
+  plugins: [new Webpack.HotModuleReplacementPlugin()]
+};
+
+module.exports = config;
