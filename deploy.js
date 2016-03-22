4cd3d0e037d209b2720620ce4d6f9ff9f5e3ac21
Sonny <sk222sw@student.lnu.se>
2016-03-22 19:11:35 +0100

    npm run deploy/deploy-windows added

--- deploy.js
+++ deploy.js
@@ -1,4 +1,5 @@
 if (process.env.NODE_ENV === "production") {
+  console.log("hej");
   var child_process = require('child_process');
   child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
