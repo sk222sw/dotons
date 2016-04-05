2d25ace7c999298d28c646663d6b1d1269ebd401
ad222kr <ad222kr@student.lnu.se>
2016-04-05 19:46:51 +0100

    mongo-db connection. initial work on dog-design schema

---
+++ credentials.js
@@ -0,0 +1,10 @@
+const mongo = {
+  development: {
+    connection_string: "mongodb://dotons:dotons2016@ds013300.mlab.com:13300/dotons"
+  },
+  production: {
+    connection_string: "mongodb://dotons:dotons2016@ds013300.mlab.com:13300/dotons"
+  }
+};
+
+module.exports = mongo;
