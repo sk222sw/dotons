768be6149bcca61a1d26728a497c7ea879165c23
Sonny <sk222sw@student.lnu.se>
2016-04-06 18:49:54 +0200

    login should work too

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ userSchema.methods.generateHash = function(password) {
 };
 
 userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.local.password);
+  return bcrypt.compareSync(password, this.password);
 };
 
 module.exports = mongoose.model("User", userSchema);
