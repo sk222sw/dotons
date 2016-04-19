abe798ecc76074fbae568b906dcbae8797327de4
Alex <ad222kr@student.lnu.se>
2016-04-19 03:00:56 +0200

    ES6 class refactor and promisify upload methods, not sure if using them right tho...

--- app/index.js
+++ app/index.js
@@ -1,42 +1,46 @@
 require("./stylesheets/style.scss");
 import request from "superagent";
-import ImageUploader from "./imageUploader";
+import ImageUploader from "./ImageUploader";
+
+// TODO: Better error-presentation for the user, flashhshhshshhs-messhahshshhages
 
 const form = document.getElementById("upload-form");
-const ctx = canvas.getContext("2d");
 
-// TODO: needs refactoring badly probably maybe
+if (form && form.addEventListener) {
+  form.addEventListener("submit", e => {
+    e.preventDefault();
 
-if (form) {
-  if (form.addEventListener) {
-    form.addEventListener("submit", e => {
-      e.preventDefault();
+    const file = document.getElementById("dot-design").files[0];
+    const target = event.explicitOriginalTarget || event.relatedTarget ||
+        document.activeElement || {}; // for knowing which submit was pressed
 
-      const file = document.getElementById("dot-design").files[0];
-      const uploadButton = document.getElementById("upload-submit");
-      const saveButton = document.getElementById("save-submit");
-      const target = event.explicitOriginalTarget || event.relatedTarget ||
-          document.activeElement || {};
+    upload(file, target);
+  }, false);
+}
 
-      if (target.value === uploadButton.value) {
-        const reader = new FileReader();
-        reader.onload = () => {
-          const output = document.createElement("img");
-          output.src = reader.result;
-          ctx.drawImage(output, 0, 0);
-        };
-        reader.readAsDataURL(file);
-      } else if (target.value === saveButton.value) {
-        const formData = new FormData();
-        formData.append("dot-design", file);
+function upload(file, target) {
+  const imageUploader = new ImageUploader();
+  const ctx = canvas.getContext("2d");
 
-        request
-          .post("/designer/upload")
-          .send(formData)
-          .end((err, res) => {
-            console.log(res.body);
-          });
-      }
-    }, false);
+  if (!file) {
+    console.log("No file chosen");
+    return;
+  }
+
+  if (target.value === form.elements["upload-submit"].value) {
+    imageUploader.uploadToClient(file)
+      .then(response => {
+        // is this how to use promises??
+        imageUploader.drawPreview(ctx, response.currentTarget.result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else if (target.value === form.elements["save-submit"].value) {
+    imageUploader.uploadToServer(file)
+      .then((response) => { console.log(response); })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
