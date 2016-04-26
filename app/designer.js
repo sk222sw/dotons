b78721ca62b847e50205ac07f2a640dc4205a6f5
Sonny <sk222sw@student.lnu.se>
2016-04-26 11:38:34 +0200

    started on designer tests and fixed promise not found in IE

--- app/designer.js
+++ app/designer.js
@@ -1,20 +1,38 @@
 /* global fabric */
 
 export default class Designer {
-  constructor(form, canvas) {
-    this.form = form;
-    console.log("contrsturcer");
+  constructor() {
+    this.imageMaxHeight = 500;
+    this.imageMaxWidth = 700;
   }
 
   initiate(base64Img) {
+    // create img node and fabric canvas object
     const imageNode = document.createElement("img");
     const c = new fabric.Canvas("canvas");
+    c.setHeight(500);
+    c.setWidth(700);
+    console.log(this.maxWidth);
     imageNode.src = base64Img;
-    console.log(imageNode);
-    const image = new fabric.Image(imageNode, {
-      left: canvas.width / 2,
-      top: canvas.height / 2
+
+    // center the image in canvas element
+    let image = new fabric.Image(imageNode, {
+      left: c.width / 2,
+      top: c.height / 2
     });
+
+    image = this.resize(image);
+
+    // add image to canvas
     c.add(image);
+  }
+
+  resize(image) {
+    if (image.width > this.imageMaxWidth || image.height > this.imageMaxHeight) {
+      image.setHeight(10);
+      image.setWidth(10);
+      return image;
+    }
+    return image;
   }
 }
