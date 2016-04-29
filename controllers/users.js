d84fe3abe1bb026ca5de5141e37b3bdeac08191e
Alex <ad222kr@student.lnu.se>
2016-04-29 03:20:33 +0200

    refactor method uploadImage to use Promise. refactor addDotDeisgnToUser in doDesignDAL to use promise -> more .then chaining -> clearer flow

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,16 @@
 const PriceListDal = require("../models/DAL/priceListDAL");
 const _ = require("lodash");
 const ROLES = require("../models/enums/roles").roles;
-const colour = require("colour");
+const uploadImage = require("../modules/uploadImage");
+const DotDesign = require("../models/dotDesign").model;
+const UPLOAD_PATH = "uploads/dot_designs/";
+const dotDesignDAL = require("../models/DAL/dotDesignDAL");
+const colour = require("colour"); // Debug shit remove later
 
 /**
  * Controller for handling users
  */
 const ctrl = function() {};
-
 
 /**
  * GET /login
@@ -48,12 +51,8 @@ ctrl.prototype.signup = function(req, res) {
   res.render("signup", context);
 
 };
-const uploadImage = require("../modules/uploadImage");
-const DotDesign = require("../models/dotDesign").model;
-const UPLOAD_PATH = "uploads/dot_designs/";
-const convertToPDF = require("../modules/convertToPDF");
-const dotDesignDAL = require("../models/DAL/dotDesignDAL");
-const create = require("../controllers/dotDesigns").create;
+
+
 
 /**
  *  GET /profile
@@ -62,125 +61,82 @@ const create = require("../controllers/dotDesigns").create;
  * @param res (description)
  */
 ctrl.prototype.profile = function(req, res, next) {
-  // TODO: NEEDS REFACTOR REALLY BADLY 
+  // TODO: NEEDS REFACTOR (NOT SO) REALLY BADLY (ANYMORE)
+  // Still DRY compared to the create action in the dotDesign-controller
+  // Move the uploading code out to separate module / BLL-class that handles it probably
   if (req.session.image) {
-    console.log("RAINBOW IMAGE IN TEH SESSION MOTHAFUCKA".red);
-    console.log(req.session.image.originalname);
-    create(req, res, next);
-    
+    // User tried to save an image but was not logged in
+    // Create the image from session and then render profile
+    const dot = new DotDesign();
+    const filenames = dot.sanitizeFilename(req.session.image.originalname);
+    dot.name = filenames.original;
+    dot.imageUrl = UPLOAD_PATH + dot.name;
+
+    uploadImage(req.session.image, dot.imageUrl)
+      .then(() => {
+        return dotDesignDAL.addDotDesignToUser(req.user.id, dot);
+      })
+      .then(() => {
+        // Image uploaded and saved to db, should be safe
+        // to push the dot to req.user.designs so the dot
+        // renders without another roundtrip to the server
+        req.user.designs.push(dot);
+        req.session.image = null;
+      })
+      .then(() => {
+        renderProfile(req.user, res);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   } else {
-    PriceListDal.getPriceList()
+    // User was already logged in, just render the view
+    req.flash("message", "Welcome back");
+    renderProfile(req.user, res);
+  }
+};
+
+
+/**
+ * Gets the pricelist and then renders the user profile
+ * Moved to separate method to eliminate DRY
+ * 
+ * @param user (description)
+ * @param res (description)
+ */
+function renderProfile(user, res, flash) {
+  PriceListDal.getPriceList()
     .then((priceList) => {
-      return getPriceByRole(priceList, req.user.role);
+      return getPriceByRole(priceList, user.role);
     })
     .then((price) => {
-      
       console.log("______RENDERING PROFILE______________".green);
       res.render("profile", {
-        email: req.user.email,
+        email: user.email,
         price,
-        designs: req.user.designs
+        designs: user.designs,
+        message: flash
       });
     })
     .catch((error) => {
       console.log(error);
     });
-  }
-  //console.log(create);
-  
-  
-  /*
-  if (req.session.image) {
-    console.log("USER PROFILE USER PROFILE USER PROFILE");
-    console.log("1. IMAGE IN SESSION".green);
-    const dot = new DotDesign();
-    const filenames = dot.sanitizeFilename(req.session.image.originalname);
-    dot.name = filenames.original;
-    dot.imageUrl = UPLOAD_PATH + dot.name;
-    console.log("2. IMAGE UPLOAD STARTED".green);
-    uploadImage(req.session.image, dot.imageUrl, error => {
-      if (error) {
-        console.log("IMAGE UPLOAD FAAAAILED".red);
-        next(error);
-      } else {
-        // BUG: cannot convert to pdf here.........
-        // convertToPDF(11, dot.name, filenames.pdf11mm, UPLOAD_PATH);
-        // convertToPDF(10, dot.name, filenames.pdf10mm, UPLOAD_PATH);
-        dotDesignDAL.addDotDesignToUser(req.user.id, dot)
-          .then(() => {
-            req.session.image = null;
-            console.log("RETURNING PRICE FROM THEN AFTER SESSUIB IMG SAVED".rainbow);
-            PriceListDal.getPriceList()
-              .then((priceList) => {
-                return getPriceByRole(priceList, req.user.role);
-              })
-              .then((price) => {
-                const designs = req.user.designs;
-                designs.push(dot);
-                console.log("______RENDERING PROFILE______________".green);
-                res.render("profile", {
-                  email: req.user.email,
-                  price,
-                  designs
-                });
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      }
-    });
-  } else {
-
-    // TODO: rename action to show?
-    PriceListDal.getPriceList()
-      .then((priceList) => {
-        return getPriceByRole(priceList, req.user.role);
-      })
-      .then((price) => {
-        console.log("______RENDERING PROFILE______________".green);
-        res.render("profile", {
-          email: req.user.email,
-          price,
-          designs: req.user.designs
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    
-    }
-    */
-};
-
-function getPriceByRole(priceList, role) {
-  var price;
-  switch (role) {
-    case ROLES.BUSINESS:
-      price = priceList.businessPrice;
-      break;
-    case ROLES.PRIVATE:
-      price = priceList.privatePrice;
-      break;
-    case ROLES.PRIVATE_RETAIL:
-      price = priceList.privateRetailsPrice;
-      break;
-    case ROLES.BUSINESS_RETAIL:
-      price = priceList.businessRetailPrice;
-      break;
-    default:
-      price = priceList.privatePrice;
-  }
-  return price;
 }
 
-function uploadImageInSession() {
-
-
-
+function getPriceByRole(priceList, role) {
+  // This should probably be a User-model instance method?
+  switch (role) {
+    case ROLES.BUSINESS:
+      return priceList.businessPrice;
+    case ROLES.PRIVATE:
+      return priceList.privatePrice;
+    case ROLES.PRIVATE_RETAIL:
+      return priceList.privateRetailsPrice;
+    case ROLES.BUSINESS_RETAIL:
+      return priceList.businessRetailPrice;
+    default:
+      return priceList.privatePrice;
+  }
 }
 
 module.exports = new ctrl();
