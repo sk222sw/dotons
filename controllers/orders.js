65e4ecb6857eb655fac9c92ed65162dc0c4f7bc7
Alex <ad222kr@student.lnu.se>
2016-05-25 21:55:01 +0200

    form to add btn to cart via ajax, posts the id n gets btn from db.

--- controllers/orders.js
+++ controllers/orders.js
@@ -10,22 +10,12 @@ ctrl.prototype.show = function(req, res) {
 
 }
 
-ctrl.prototype.new = function(req, res) {
-  res.render("newOrder", {
-    title: "dotons - create order",
-    message: req.flash("Hello!"),
-    csrfToken: req.csrfToken()
-  });
-}
+
 
 ctrl.prototype.create = function(req, res) {
   console.log("Hehe created order kinda");
   res.redirect("/profile");
 };
 
-
-ctrl.prototype.addToOrder = function(req, res) {
-
-};
 
 module.exports = new ctrl();
--- models/DAL/dotDesignDAL.js
+++ models/DAL/dotDesignDAL.js
@@ -13,7 +13,7 @@ const dotDesignDAL = {
 
   /**
    * Adds a dot design to the current user
-   * 
+   *
    *
    * @param {String} userid - id of the current user
    * @param {DotDesign} dotDesign - to be added
@@ -34,6 +34,21 @@ const dotDesignDAL = {
 
   getUserDesigns: (userid) => {
     return User.findById(userid).exec();
+  },
+
+  getUserDesignByID: (userID, designID) => {
+    return new Promise((resolve, reject) => {
+      User.findById(userID, (err, user) => {
+        if (err) return reject(err);
+        var design = user.designs.find((des) => {
+          console.log(typeof des.id);
+          console.log(typeof designID);
+          return des.id === designID;
+        });
+        
+        console.log(design);
+      });
+    });
   }
 
 
