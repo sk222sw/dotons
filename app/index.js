057ec6583de8d792009c51bef57cae4fb0e49caf
ad222kr <ad222kr@student.lnu.se>
2016-05-27 00:12:15 +0200

    cart bugfixes. cart works on a backend level now

--- app/index.js
+++ app/index.js
@@ -9,28 +9,50 @@ const addToCartForms = document.getElementsByClassName("add-button-form");
 
 if (addToCartForms) { // more than the login forms
   _.each(addToCartForms, (element, index) => {
-    console.log(element);
     element.addEventListener("submit", e => {
       e.preventDefault();
-      console.log("ADDED btn to cart");
-      console.log(element);
-      element.add.disabled = true;
 
-      request
+      
+      if (element.order.classList.contains("add")) {
+        request
         .post("/add")
         .send({ buttonID: element.buttonID.value, _csrf: element._csrf.value })
         .withCredentials()
         .end((err, res) => {
           if (err) console.log(err); // handle error
           const response = JSON.parse(res.text);
-          console.log(response.success);
+          console.log(response);
           if (response.success) {
-            console.log(response.cart);
+            console.log("added to cart");
+            element.order.value = "Remove from cart"
+            element.order.classList.remove("add");
+            element.order.classList.add("remove");
+
             // do something with the cart yao
           } else {
             // Could not add, present error
           }
-        });
+        });  
+      } else if (element.order.classList.contains("remove")) {
+        request
+          .post("/remove")
+          .send({ buttonID: element.buttonID.value, _csrf: element._csrf.value })
+          .withCredentials()
+          .end((err, res) => {
+            const response = JSON.parse(res.text);
+            console.log(response);
+            if(err) console.log(err) // handle error
+            if (response.success) {
+              console.log("removed from cart");
+              
+              element.order.classList.add("add");
+              element.order.classList.remove("remove");
+              element.order.value = "Add to cart";
+            }
+          });
+        
+      }
+      
     });
   });
 }
