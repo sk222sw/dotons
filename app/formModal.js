5aee00d25f1bf026789f47faf4c08d79b6b34803
ad222kr <ad222kr@student.lnu.se>
2016-05-28 19:15:09 +0200

    Remove items from cart, update order

--- app/formModal.js
+++ app/formModal.js
@@ -3,11 +3,14 @@ const loginModal = document.getElementById("modal");
 const modalContainer = document.getElementById("form-modal-container");
 const closeLoginModal = document.getElementById("button-close-form-modal");
 
-closeLoginModal.addEventListener("click", () => {
-  modalContainer.classList.add("hidden");
-  console.log(modalContainer);
-  console.log("hehehehehehehehe");
-});
+if (closeLoginModal) {
+  closeLoginModal.addEventListener("click", () => {
+    modalContainer.classList.add("hidden");
+    console.log(modalContainer);
+    console.log("hehehehehehehehe");
+  });
+}
+
 if (loginButton) {
   loginButton.addEventListener("click", () => {
     modalContainer.classList.toggle('hidden');
