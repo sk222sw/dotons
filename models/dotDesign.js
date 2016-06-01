67c60ec4936d87cf6d9e3e2f944d83cc095d71e4
Alex <ad222kr@student.lnu.se>
2016-06-01 17:08:19 +0200

    some test fix. no time to fix POST-tests to work with csrftoken/

--- models/dotDesign.js
+++ models/dotDesign.js
@@ -20,18 +20,18 @@ dotDesignSchema.methods.sanitizeFilename = function(filename) {
   const parts = filename.split(".");
   // const ext = parts[parts.length - 1];
   const name = parts.splice(0, parts.length - 1).join(".");
-  const sanitized = name.replace(/[^a-z0-9]/gi, "_").toLowerCase() + Date.now();
+  const sanitized = name.replace(/[^a-z0-9]/gi, "_").toLowerCase() + "." + Date.now();
 
-  fs.stat("uploads/dot_designs/" + sanitized + ".png", (err, stat) => {
-    if (err == null) {
-      console.log("File exists!");
-    } else if (err.code === "ENOENT") {
-      // file does not exists
-      console.log("File does not exist");
-    } else {
-      console.log("Some other error: ", err.code);
-    }
-  });
+  // fs.stat("uploads/dot_designs/" + sanitized + ".png", (err, stat) => {
+  //   if (err == null) {
+  //     console.log("File exists!");
+  //   } else if (err.code === "ENOENT") {
+  //     // file does not exists
+  //     console.log("File does not exist");
+  //   } else {
+  //     console.log("Some other error: ", err.code);
+  //   }
+  // });
 
   const ret = {
     original: sanitized + ".png", // all images stored as png
